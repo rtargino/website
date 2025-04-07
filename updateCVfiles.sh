5c91ff1456814117d96f6e880cdbcaaf9bed840e
Rodrigo Targino <targinorj@gmail.com>
2025-04-07 19:40:39 -0300

    remove short cv

--- updateCVfiles.sh
+++ updateCVfiles.sh
@@ -1,6 +1,5 @@
 pip install -r ./src/requirements.txt
 python ./src/bibtex_processor.py
 
-Rscript -e 'pandoc::pandoc_activate(); rmarkdown::render("./src/RodrigoSTarginoCV.Rmd"); rmarkdown::render("./src/RodrigoSTarginoCV_short.Rmd")'
+Rscript -e 'pandoc::pandoc_activate(); rmarkdown::render("./src/RodrigoSTarginoCV.Rmd")'
 mv src/RodrigoSTarginoCV.pdf static/author_data/RodrigoSTarginoCV.pdf
-mv src/RodrigoSTarginoCV_short.pdf static/author_data/RodrigoSTarginoCV_short.pdf
